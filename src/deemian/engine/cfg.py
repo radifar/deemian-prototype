5bb27d831ccff2642f15abea7edd0164fc53042b
radifar <m.radifar05@gmail.com>
2023-04-17 20:36:37 +0700

    refactor: simplify grammar, tree, and test

--- src/deemian/engine/cfg.py
+++ src/deemian/engine/cfg.py
@@ -3,5 +3,5 @@ from lark import Lark
 
 def parser(text):
     """Generate parser using Lark and grammar then parse the input text"""
-    lark_parser = Lark.open("grammar.lark", rel_to=__file__, parser="lalr")
+    lark_parser = Lark.open("grammar.lark", rel_to=__file__, parser="lalr", start="statement")
     return lark_parser.parse(text)
--- tests/test_engine_cfg.py
+++ tests/test_engine_cfg.py
@@ -7,24 +7,19 @@ def test_parser_protein():
     >>> from deemian.engine.cfg import parser
     >>> selection = parser("protein in zinc_finger")
     >>> rich.print(selection)
-    start
-    └── selection
-        ├── selection_string
-        │   └── protein
-        ├── in
-        └── zinc_finger
+    selection
+    ├── selection_string
+    │   └── protein
+    ├── in
+    └── zinc_finger
     >>> selection
-    Tree(Token('RULE', 'start'),
-      [Tree(Token('RULE', 'selection'),
-        [Tree(Token('RULE', 'selection_string'),
-          [Token('MACRO', 'protein')]),
-        Token('PREPOSITION', 'in'),
-        Token('IDENTIFIER', 'zinc_finger')])])
+    [Tree(Token('RULE', 'selection'),
+      [Tree(Token('RULE', 'selection_string'),
+        [Token('MACRO', 'protein')]),
+      Token('PREPOSITION', 'in'),
+      Token('IDENTIFIER', 'zinc_finger')])])
     """
-    selection = "protein in zinc_finger"
-
-    result = cfg.parser(selection)
-    selection = result.children[0]
+    selection = cfg.parser("protein in zinc_finger")
     selection_string, preposition, identifier = selection.children
     macro = selection_string.children[0]
 
@@ -41,28 +36,23 @@ def test_parser_selection_protein_or_metal():
     >>> from deemian.engine.cfg import parser
     >>> selection = parser("protein OR metal in zinc_finger")
     >>> rich.print(selection)
-    start
-    └── selection
-        ├── selection_string
-        │   └── protein
-        │   └── OR
-        │   └── metal
-        ├── in
-        └── zinc_finger
+    selection
+    ├── selection_string
+    │   └── protein
+    │   └── OR
+    │   └── metal
+    ├── in
+    └── zinc_finger
     >>> selection
-    Tree(Token('RULE', 'start'),
-      [Tree(Token('RULE', 'selection'),
-        [Tree(Token('RULE', 'selection_string'),
-          [Token('MACRO', 'protein'),
-          Token('LOGIC', 'OR'),
-          Token('MACRO', 'metal')]),
-        Token('PREPOSITION', 'in'),
-        Token('IDENTIFIER', 'zinc_finger')])])
+    [Tree(Token('RULE', 'selection'),
+      [Tree(Token('RULE', 'selection_string'),
+        [Token('MACRO', 'protein'),
+        Token('LOGIC', 'OR'),
+        Token('MACRO', 'metal')]),
+      Token('PREPOSITION', 'in'),
+      Token('IDENTIFIER', 'zinc_finger')])])
     """
-    selection = "protein OR metal in zinc_finger"
-
-    result = cfg.parser(selection)
-    selection = result.children[0]
+    selection = cfg.parser("protein OR metal in zinc_finger")
     selection_string, preposition, identifier = selection.children
     macro1, logic, macro2 = selection_string.children
 
@@ -81,24 +71,19 @@ def test_parser_selection_ligand():
     >>> from deemian.engine.cfg import parser
     >>> selection = parser("ligand in zinc_finger")
     >>> rich.print(selection)
-    start
-    └── selection
-        ├── selection_string
-        │   └── ligand
-        ├── in
-        └── zinc_finger
+    selection
+    ├── selection_string
+    │   └── ligand
+    ├── in
+    └── zinc_finger
     >>> selection
-    Tree(Token('RULE', 'start'),
-      [Tree(Token('RULE', 'selection'),
-        [Tree(Token('RULE', 'selection_string'),
-          [Token('MACRO', 'ligand')]),
-        Token('PREPOSITION', 'in'),
-        Token('IDENTIFIER', 'zinc_finger')])])
+    [Tree(Token('RULE', 'selection'),
+      [Tree(Token('RULE', 'selection_string'),
+        [Token('MACRO', 'ligand')]),
+      Token('PREPOSITION', 'in'),
+      Token('IDENTIFIER', 'zinc_finger')])])
     """
-    selection = "ligand in zinc_finger"
-
-    result = cfg.parser(selection)
-    selection = result.children[0]
+    selection = cfg.parser("ligand in zinc_finger")
     selection_string, preposition, identifier = selection.children
     macro = selection_string.children[0]
 
@@ -115,30 +100,25 @@ def test_parser_measurement():
     >>> from deemian.engine.cfg import parser
     >>> measurement = parser("nonpolar on protein")
     >>> rich.print(measurement)
-    start
-    └── measurement
-        ├── interactions
-        │   └── nonpolar
-        └── target
-            ├── on
-            ├── protein
-            ├── None
-            └── None
+    measurement
+    ├── interactions
+    │   └── nonpolar
+    └── target
+        ├── on
+        ├── protein
+        ├── None
+        └── None
     >>> measurement
-    Tree(Token('RULE', 'start'),
-      [Tree(Token('RULE', 'measurement'),
-        [Tree(Token('RULE', 'interactions'),
-          [Token('INTERACTION', 'nonpolar')]),
-        Tree(Token('RULE', 'target'),
-          [Token('PREPOSITION', 'on'),
-          Token('IDENTIFIER', 'protein'),
-          None,
-          None])])])
+    [Tree(Token('RULE', 'measurement'),
+      [Tree(Token('RULE', 'interactions'),
+        [Token('INTERACTION', 'nonpolar')]),
+      Tree(Token('RULE', 'target'),
+        [Token('PREPOSITION', 'on'),
+        Token('IDENTIFIER', 'protein'),
+        None,
+        None])])])
     """
-    measurement = "nonpolar on protein"
-
-    result = cfg.parser(measurement)
-    measurement = result.children[0]
+    measurement = cfg.parser("nonpolar on protein")
     interaction, target = measurement.children
 
     assert measurement.data == "measurement"
@@ -157,34 +137,29 @@ def test_parser_measurement_multi_interaction():
     >>> from deemian.engine.cfg import parser
     >>> measurement = parser("nonpolar, electrostatic, hydrogen_bond on protein")
     >>> rich.print(measurement)
-    start
-    └── measurement
-        ├── interactions
-        │   ├── nonpolar
-        │   ├── electrostatic
-        │   └── hydrogen_bond
-        └── target
-            ├── on
-            ├── protein
-            ├── None
-            └── None
+    measurement
+    ├── interactions
+    │   ├── nonpolar
+    │   ├── electrostatic
+    │   └── hydrogen_bond
+    └── target
+        ├── on
+        ├── protein
+        ├── None
+        └── None
     >>> measurement
-    Tree(Token('RULE', 'start'),
-      [Tree(Token('RULE', 'measurement'),
-        [Tree(Token('RULE', 'interactions'),
-          [Token('INTERACTION', 'nonpolar'),
-          Token('INTERACTION', 'electrostatic'),
-          Token('INTERACTION', 'hydrogen_bond')]),
-        Tree(Token('RULE', 'target'),
-          [Token('PREPOSITION', 'on'),
-          Token('IDENTIFIER', 'protein'),
-          None,
-          None])])])
+    [Tree(Token('RULE', 'measurement'),
+      [Tree(Token('RULE', 'interactions'),
+        [Token('INTERACTION', 'nonpolar'),
+        Token('INTERACTION', 'electrostatic'),
+        Token('INTERACTION', 'hydrogen_bond')]),
+      Tree(Token('RULE', 'target'),
+        [Token('PREPOSITION', 'on'),
+        Token('IDENTIFIER', 'protein'),
+        None,
+        None])])])
     """
-    measurement = "nonpolar, electrostatic, hydrogen_bond on protein"
-
-    result = cfg.parser(measurement)
-    measurement = result.children[0]
+    measurement = cfg.parser("nonpolar, electrostatic, hydrogen_bond on protein")
     interaction, target = measurement.children
 
     assert interaction.children[0] == "nonpolar"
@@ -198,30 +173,25 @@ def test_parser_measurement_multi_target():
     >>> from deemian.engine.cfg import parser
     >>> measurement = parser("nonpolar between protein and ligand")
     >>> rich.print(measurement)
-    start
-    └── measurement
-        ├── interactions
-        │   └── nonpolar
-        └── target
-            ├── between
-            ├── protein
-            ├── and
-            └── ligand
+    measurement
+    ├── interactions
+    │   └── nonpolar
+    └── target
+        ├── between
+        ├── protein
+        ├── and
+        └── ligand
     >>> measurement
-    Tree(Token('RULE', 'start'),
-      [Tree(Token('RULE', 'measurement'),
-        [Tree(Token('RULE', 'interactions'),
-          [Token('INTERACTION', 'nonpolar')]),
-        Tree(Token('RULE', 'target'),
-          [Token('PREPOSITION', 'between'),
-          Token('IDENTIFIER', 'protein'),
-          Token('PREPOSITION', 'and'),
-          Token('IDENTIFIER', 'ligand')])])])
+    [Tree(Token('RULE', 'measurement'),
+      [Tree(Token('RULE', 'interactions'),
+        [Token('INTERACTION', 'nonpolar')]),
+      Tree(Token('RULE', 'target'),
+        [Token('PREPOSITION', 'between'),
+        Token('IDENTIFIER', 'protein'),
+        Token('PREPOSITION', 'and'),
+        Token('IDENTIFIER', 'ligand')])])])
     """
-    measurement = "nonpolar between protein and ligand"
-
-    result = cfg.parser(measurement)
-    measurement = result.children[0]
+    measurement = cfg.parser("nonpolar between protein and ligand")
     interaction, target = measurement.children
 
     assert target.children[0] == "between"
