5924739e490fc0572c90af7e3a0b9c72f5410086
radifar <m.radifar05@gmail.com>
2023-04-18 10:33:13 +0700

    test: refine molecule_loader test

---
+++ tests/test_engine_processor.py
@@ -0,0 +1,27 @@
+from rdkit import Chem
+
+from deemian.engine import model, processor
+
+
+def test_load_molecule_processor(simple_yaml_parsed):
+    specification = model.Specification.parse_obj(simple_yaml_parsed)
+    molecules = specification.load_molecule
+    molecule_id = [key for key in molecules.keys()][0]
+
+    loaded_molecules = processor.molecule_loader(molecules)
+    loaded_molecule = loaded_molecules[molecule_id]
+    get_atom_num = loaded_molecule.GetNumAtoms()
+
+    assert isinstance(loaded_molecule, Chem.rdchem.Mol)
+    assert get_atom_num == 220
+
+
+def test_selection_processor():
+    # TODO: Create fixture for RDKit.mol of PDB example
+    # TODO: Selection Model --> fill with selection_string, preposition, identifier
+    # TODO: Add Selection method on Model that generate the corresponding selection (RDKit.mol)
+    pass
+
+
+def test_measurement_processor():
+    pass
